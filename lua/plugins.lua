bf7f9a29b476f40a868deccbe1816a035d7c2ce6
Quilled <alytarfa@gmail.com>
2023-12-30 19:06:19 -0500

    add indent guide lines plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -69,6 +69,60 @@ local plugins = {
 	"folke/todo-comments.nvim",
 	"goolord/alpha-nvim",
 	"stevearc/dressing.nvim",
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+			},
+		},
+		main = "ibl",
+	},
+	{
+		"echasnovski/mini.indentscope",
+		opts = {
+			-- symbol = "▏",
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+	},
 
 	"lervag/vimtex",
 }
