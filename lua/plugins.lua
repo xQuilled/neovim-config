7d50c2d4f6da8321a880588fdbb1c320dfd6bddd
Quilled <alytarfa@gmail.com>
2024-01-07 23:56:35 -0500

    refactor: move indent plugin opts into own plugin file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -74,60 +74,8 @@ local plugins = {
 	"goolord/alpha-nvim",
 	"rcarriga/nvim-notify",
 	"stevearc/dressing.nvim",
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		opts = {
-			indent = {
-				char = "│",
-				tab_char = "│",
-			},
-			scope = { enabled = false },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-			},
-		},
-		main = "ibl",
-	},
-	{
-		"echasnovski/mini.indentscope",
-		opts = {
-			-- symbol = "▏",
-			symbol = "│",
-			options = { try_as_border = true },
-		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
-	},
+	"lukas-reineke/indent-blankline.nvim",
+	"echasnovski/mini.indentscope",
 
 	"lervag/vimtex",
 }
