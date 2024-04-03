ee1f7d46649080b74238a28bd20db3bc785c6b6b
Quilled <alytarfa@gmail.com>
2024-04-02 21:52:15 -0400

    change: round borders for toggleterm

--- lua/config/buffers.lua
+++ lua/config/buffers.lua
@@ -19,12 +19,7 @@ require("toggleterm").setup({
 	open_mapping = [[<F7>]],
 	auto_scroll = true,
 	direction = "float",
-	-- float_opts = {
-	-- 	border = "curved",
-	-- 	winblend = 3,
-	-- 	highlights = {
-	-- 		border = "Normal",
-	-- 		background = "Normal",
-	-- 	},
-	-- },
+	float_opts = {
+		border = "curved",
+	},
 })
